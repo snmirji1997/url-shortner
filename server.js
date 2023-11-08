edb8b49bcf8430679cfee77bcb2bc71b261c92bf
snmirji1997 <snmirji1997@gmail.com>
2023-11-08 15:30:08 +0530

    schema defined

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const shortUrl = require('./models/shortUrl')
+
 const app = express();
+mongoose.connect('mongodb://localhost/urlShortner',{
+    useNewUrlParser:true,useUnifiedTopology:true
+})
 
 app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
     res.render('index')
+})
+
+app.post("./shortUrl",(req,res)=>{
+    
+
 })
 app.listen(process.env.PORT || 5000)
